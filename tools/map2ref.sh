86148d4cc950138f6ccd05de5f4df4b408d9169e
Efrat Muller <em2035@login-q-3.data.cluster>
2024-12-13 13:43:30 +0000

    prelim changes

--- tools/map2ref.sh
+++ tools/map2ref.sh
@@ -81,9 +81,11 @@ echo "$(timestamp) [ map2ref pipeline ] Running BWA ..."
 rm -f ${outprefix}_${refix}*bam*
 bwa mem -M -t ${threads} ${ref} ${reads} ${reads2} | samtools view -@ ${threads_sam} -F 256 -uS - | samtools sort -@ ${threads_sam} - -o ${outprefix}_${refix}_raw.bam
 samtools index -@ ${threads_sam} ${outprefix}_${refix}_raw.bam
+
+# keep only alignments where >60% of the read had >90% identity to the reference
 tools/bam_ani-filter.py ${outprefix}_${refix}_raw.bam 90 60 | samtools view -u - -o ${outprefix}_${refix}_sorted.bam
 
-# extract unique counts
+# extract unique counts (reads mapped to a single reference)
 echo "$(timestamp) [ map2ref pipeline ] Extracting unique counts ..."
 samtools view -@ ${threads_sam} -q 1 -f 2 -u ${outprefix}_${refix}_sorted.bam -o ${outprefix}_${refix}_unique_sorted.bam
 samtools index -@ ${threads_sam} ${outprefix}_${refix}_unique_sorted.bam
@@ -94,11 +96,19 @@ samtools idxstats ${outprefix}_${refix}_unique_sorted.bam > ${outprefix}_${refix
 samtools depth ${outprefix}_${refix}_unique_sorted.bam > ${outprefix}_${refix}_unique_depth-pos.tab
 tools/parse_bwa-depth.py ${outprefix}_${refix}_unique_depth.tab ${outprefix}_${refix}_unique_depth-pos.tab ${mode} _${refix} > ${outprefix}_${refix}_unique.tab
 
-# parse total count output
+# parse total count output (this time including reads mapped to multiple references but randomly assigned to one)
 samtools index -@ ${threads_sam} ${outprefix}_${refix}_sorted.bam
 samtools idxstats ${outprefix}_${refix}_sorted.bam > ${outprefix}_${refix}_depth.tab
 samtools depth ${outprefix}_${refix}_sorted.bam > ${outprefix}_${refix}_depth-pos.tab
 tools/parse_bwa-depth.py ${outprefix}_${refix}_depth.tab ${outprefix}_${refix}_depth-pos.tab ${mode} _${refix} > ${outprefix}_${refix}_total.tab
+
+# save a list of aligned reads
+# (to reduce space, each read-pair is noted once and unique mappings are saved seperatly from non-unique mappings)
+# samtools view ${outprefix}_${refix}_sorted.bam | cut -f 1,2,5 | gzip -c > ${outprefix}_${refix}_aligned_reads.tab.gz
+samtools view ${outprefix}_${refix}_unique_sorted.bam | cut -f 1 | sort | uniq > ${outprefix}_${refix}_aligned_reads_uniq.txt
+samtools view ${outprefix}_${refix}_sorted.bam | cut -f 1 | sort | uniq | comm -13 ${outprefix}_${refix}_aligned_reads_uniq.txt - > ${outprefix}_${refix}_aligned_reads_non_uniq.txt
+gzip ${outprefix}_${refix}_aligned_reads_uniq.txt
+gzip ${outprefix}_${refix}_aligned_reads_non_uniq.txt
 
 # clean tmp files
 echo "$(timestamp) [ map2ref pipeline ] Cleaning tmp files ..."
