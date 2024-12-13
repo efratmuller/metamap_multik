3acc5dbc114a3a3a072bf321a10e8d8fac8e9622
Efrat Muller <em2035@login-q-3.data.cluster>
2024-12-13 14:46:54 +0000

    expanded mapping to two databases - WIP

--- Snakefile
+++ Snakefile
@@ -1,4 +1,4 @@
-# snakemake workflow for mapping metagenomes against reference database
+# snakemake workflow for mapping metagenomes against 2 reference databases (herein refered to as database A and database B)
 
 import os
 
@@ -7,10 +7,12 @@ configfile: "config.yml"
 
 INPUT_FILE = config["input"]
 OUTPUT_DIR = config["output"]
-REFNAME = os.path.basename(config["database"]["bwa_db"]).split(".fa")[0]
+REF_NAME_A = config["database_a"]["db_name"]
+REF_NAME_B = config["database_b"]["db_name"]
 
 os.system("chmod -R +x tools")
 
+# parse the list of samples and paths to fastqs
 samp2path = {}
 with open(INPUT_FILE) as f:
     for line in f:
@@ -20,33 +22,56 @@ with open(INPUT_FILE) as f:
         sample = os.path.basename(cols[0]).split("_1.fastq")[0]
         samp2path[sample] = [fwd, rev]
 
+# create folders required for later
 for sample in samp2path.keys():
     if not os.path.exists(OUTPUT_DIR+"/mapping/"+sample+"/logs"):
         os.makedirs(OUTPUT_DIR+"/mapping/"+sample+"/logs")
-
 if not os.path.exists(OUTPUT_DIR+"/summary/logs"):
     os.makedirs(OUTPUT_DIR+"/summary/logs")
 
 # rule that specifies the final expected output files
 rule all:
     input:
-        OUTPUT_DIR+"/summary/bwa_cov-est.csv",
-        OUTPUT_DIR+"/summary/bwa_cov-exp.csv",
-        OUTPUT_DIR+"/summary/bwa_counts-total.csv",
-        OUTPUT_DIR+"/summary/bwa_counts-unique.csv"
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_A+"_cov-est.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_A+"_cov-exp.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_A+"_counts-total.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_A+"_counts-unique.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_B+"_cov-est.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_B+"_cov-exp.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_B+"_counts-total.csv",
+        OUTPUT_DIR+"/summary/bwa_"+REF_NAME_B+"_counts-unique.csv"
 
-# map metagenomes
-rule map2ref:
+# map metagenomes to catalog A
+rule map2ref_a:
     input:
         fwd = lambda wildcards: samp2path[wildcards.sample][0],
         rev = lambda wildcards: samp2path[wildcards.sample][1]
     output:
-        OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REFNAME+"_total.tab",
-        OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REFNAME+"_unique.tab"
+        OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_A+"_total.tab",
+        OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_A+"_unique.tab"
     params:
-        outpref = OUTPUT_DIR+"/mapping/{sample}/{sample}",
-        bwa_db = config["database"]["bwa_db"],
-        reftype = config["database"]["type"]
+        outpref = OUTPUT_DIR+"/mapping/{sample}/{sample}_" + REF_NAME_A,
+        bwa_db = config["database_a"]["bwa_db"],
+        reftype = config["database_a"]["type"]
+    conda:
+        "envs/metamap.yml"
+    shell:
+        """
+        tools/map2ref.sh -t 16 -i {input.fwd} -n {input.rev} -r {params.bwa_db} -o {params.outpref} -c {params.reftype}
+        """
+
+# map metagenomes to catalog B
+rule map2ref_b:
+    input:
+        fwd = lambda wildcards: samp2path[wildcards.sample][0],
+        rev = lambda wildcards: samp2path[wildcards.sample][1]
+    output:
+        OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_B+"_total.tab",
+        OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_B+"_unique.tab"
+    params:
+        outpref = OUTPUT_DIR+"/mapping/{sample}/{sample}_" + REF_NAME_B,
+        bwa_db = config["database_b"]["bwa_db"],
+        reftype = config["database_b"]["type"]
     conda:
         "envs/metamap.yml"
     shell:
@@ -57,7 +82,7 @@ rule map2ref:
 # parse final output
 rule parse_cov:
     input:
-        expand(OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REFNAME+"_total.tab", sample=samp2path.keys())
+        expand(OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_A+"_total.tab", sample=samp2path.keys())
     output:
         OUTPUT_DIR+"/summary/bwa_cov-est.csv"
     params:
@@ -71,7 +96,7 @@ rule parse_cov:
 
 rule parse_expcov:
     input:
-        expand(OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REFNAME+"_total.tab", sample=samp2path.keys())
+        expand(OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_A+"_total.tab", sample=samp2path.keys())
     output:
         OUTPUT_DIR+"/summary/bwa_cov-exp.csv"
     params:
@@ -85,7 +110,7 @@ rule parse_expcov:
 
 rule parse_counts:
     input:
-        lambda wildcards: expand(OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REFNAME+"_{type}.tab", sample=samp2path.keys(), type=wildcards.type)
+        lambda wildcards: expand(OUTPUT_DIR+"/mapping/{sample}/{sample}_"+REF_NAME_A+"_{type}.tab", sample=samp2path.keys(), type=wildcards.type)
     output:
         OUTPUT_DIR+"/summary/bwa_counts-{type}.csv"
     params:
--- tools/map2ref.sh
+++ tools/map2ref.sh
@@ -26,7 +26,7 @@ reads2=
 outprefix=
 mode=
 
-while getopts “t:m:i:n:r:o:c:” OPTION
+while getopts “t:i:n:r:o:c:” OPTION
 do
      case ${OPTION} in
          t)
@@ -68,7 +68,7 @@ timestamp() {
 }
 
 readname=$(basename ${outprefix})
-refix=$(basename ${ref%%.fa*})
+
 if [ ${threads} -eq 1 ]
 then
     threads_sam=1
@@ -78,39 +78,39 @@ fi
 
 # initial mapping and sorting
 echo "$(timestamp) [ map2ref pipeline ] Running BWA ..."
-rm -f ${outprefix}_${refix}*bam*
-bwa mem -M -t ${threads} ${ref} ${reads} ${reads2} | samtools view -@ ${threads_sam} -F 256 -uS - | samtools sort -@ ${threads_sam} - -o ${outprefix}_${refix}_raw.bam
-samtools index -@ ${threads_sam} ${outprefix}_${refix}_raw.bam
+rm -f ${outprefix}*bam*
+bwa mem -M -t ${threads} ${ref} ${reads} ${reads2} | samtools view -@ ${threads_sam} -F 256 -uS - | samtools sort -@ ${threads_sam} - -o ${outprefix}_raw.bam
+samtools index -@ ${threads_sam} ${outprefix}_raw.bam
 
 # keep only alignments where >60% of the read had >90% identity to the reference
-tools/bam_ani-filter.py ${outprefix}_${refix}_raw.bam 90 60 | samtools view -u - -o ${outprefix}_${refix}_sorted.bam
+tools/bam_ani-filter.py ${outprefix}_raw.bam 90 60 | samtools view -u - -o ${outprefix}_sorted.bam
 
 # extract unique counts (reads mapped to a single reference)
 echo "$(timestamp) [ map2ref pipeline ] Extracting unique counts ..."
-samtools view -@ ${threads_sam} -q 1 -f 2 -u ${outprefix}_${refix}_sorted.bam -o ${outprefix}_${refix}_unique_sorted.bam
-samtools index -@ ${threads_sam} ${outprefix}_${refix}_unique_sorted.bam
+samtools view -@ ${threads_sam} -q 1 -f 2 -u ${outprefix}_sorted.bam -o ${outprefix}_unique_sorted.bam
+samtools index -@ ${threads_sam} ${outprefix}_unique_sorted.bam
 
 # parse unique count output
 echo "$(timestamp) [ map2ref pipeline ] Parsing results ..."
-samtools idxstats ${outprefix}_${refix}_unique_sorted.bam > ${outprefix}_${refix}_unique_depth.tab
-samtools depth ${outprefix}_${refix}_unique_sorted.bam > ${outprefix}_${refix}_unique_depth-pos.tab
-tools/parse_bwa-depth.py ${outprefix}_${refix}_unique_depth.tab ${outprefix}_${refix}_unique_depth-pos.tab ${mode} _${refix} > ${outprefix}_${refix}_unique.tab
+samtools idxstats ${outprefix}_unique_sorted.bam > ${outprefix}_unique_depth.tab
+samtools depth ${outprefix}_unique_sorted.bam > ${outprefix}_unique_depth-pos.tab
+tools/parse_bwa-depth.py ${outprefix}_unique_depth.tab ${outprefix}_unique_depth-pos.tab ${mode} _${refix} > ${outprefix}_unique.tab
 
 # parse total count output (this time including reads mapped to multiple references but randomly assigned to one)
-samtools index -@ ${threads_sam} ${outprefix}_${refix}_sorted.bam
-samtools idxstats ${outprefix}_${refix}_sorted.bam > ${outprefix}_${refix}_depth.tab
-samtools depth ${outprefix}_${refix}_sorted.bam > ${outprefix}_${refix}_depth-pos.tab
-tools/parse_bwa-depth.py ${outprefix}_${refix}_depth.tab ${outprefix}_${refix}_depth-pos.tab ${mode} _${refix} > ${outprefix}_${refix}_total.tab
+samtools index -@ ${threads_sam} ${outprefix}_sorted.bam
+samtools idxstats ${outprefix}_sorted.bam > ${outprefix}_depth.tab
+samtools depth ${outprefix}_sorted.bam > ${outprefix}_depth-pos.tab
+tools/parse_bwa-depth.py ${outprefix}_depth.tab ${outprefix}_depth-pos.tab ${mode} _${refix} > ${outprefix}_total.tab
 
 # save a list of aligned reads
 # (to reduce space, each read-pair is noted once and unique mappings are saved seperatly from non-unique mappings)
-# samtools view ${outprefix}_${refix}_sorted.bam | cut -f 1,2,5 | gzip -c > ${outprefix}_${refix}_aligned_reads.tab.gz
-samtools view ${outprefix}_${refix}_unique_sorted.bam | cut -f 1 | sort | uniq > ${outprefix}_${refix}_aligned_reads_uniq.txt
-samtools view ${outprefix}_${refix}_sorted.bam | cut -f 1 | sort | uniq | comm -13 ${outprefix}_${refix}_aligned_reads_uniq.txt - > ${outprefix}_${refix}_aligned_reads_non_uniq.txt
-gzip ${outprefix}_${refix}_aligned_reads_uniq.txt
-gzip ${outprefix}_${refix}_aligned_reads_non_uniq.txt
+# samtools view ${outprefix}_sorted.bam | cut -f 1,2,5 | gzip -c > ${outprefix}_aligned_reads.tab.gz
+samtools view ${outprefix}_unique_sorted.bam | cut -f 1 | sort | uniq > ${outprefix}_aligned_reads_uniq.txt
+samtools view ${outprefix}_sorted.bam | cut -f 1 | sort | uniq | comm -13 ${outprefix}_aligned_reads_uniq.txt - > ${outprefix}_aligned_reads_non_uniq.txt
+gzip ${outprefix}_aligned_reads_uniq.txt
+gzip ${outprefix}_aligned_reads_non_uniq.txt
 
 # clean tmp files
 echo "$(timestamp) [ map2ref pipeline ] Cleaning tmp files ..."
-rm -rf ${outprefix}_${refix}_unique_sorted.ba* ${outprefix}_${refix}_unique_depth*
-rm -rf ${outprefix}_${refix}_raw.ba* ${outprefix}_${refix}_unsorted.bam ${outprefix}_${refix}_sorted.ba* ${outprefix}_${refix}_depth*
+rm -rf ${outprefix}_unique_sorted.ba* ${outprefix}_unique_depth*
+rm -rf ${outprefix}_raw.ba* ${outprefix}_unsorted.bam ${outprefix}_sorted.ba* ${outprefix}_depth*
